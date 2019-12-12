392ffcda63f955922a1d1ae060a44195f088cb2b
w3bdesign <hijack_xxx@yahoo.com>
2019-12-12 08:10:31 +0100

    Heroku deployment

---
+++ app/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var fetch = require("node-fetch");
+var app = express();
+var port = process.env.PORT || 8080;
+var cors = require("cors");
+app.use(cors());
+app.listen(port);
+
+// routes will go here
+app.get("/bil/:nummer", function(req, res) {
+  if (req.params.nummer) {
+    fetch(
+      `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${req.params.nummer}`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => console.log(err));
+  }
+});
