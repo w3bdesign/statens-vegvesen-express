c979bf4dadb215da7de0f7f81ca87a3fd99eb331
w3bdesign <hijack_xxx@yahoo.com>
2020-05-04 03:41:29 +0200

    Prettier

--- app/server.js
+++ app/server.js
@@ -1,31 +1,30 @@
-var express = require("express");
-var helmet = require("helmet");
-var fetch = require("node-fetch");
+var express = require('express');
+var helmet = require('helmet');
+var fetch = require('node-fetch');
 var app = express();
 var port = process.env.PORT || 8080;
-var cors = require("cors");
+var cors = require('cors');
 
 // Set allowed URL for CORS
 const corsOptions = {
-  origin: "https://sleepy-payne-cbfe8b.netlify.app"
+  origin: 'https://sleepy-payne-cbfe8b.netlify.app',
 };
 
 app.use(helmet());
 app.use(cors(corsOptions));
 app.listen(port);
 
-// routes will go here
-app.get("/bil/:nummer", function(req, res) {
+app.get('/bil/:nummer', function (req, res) {
   if (req.params.nummer) {
     fetch(
       `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${req.params.nummer}`
     )
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         res.send(data);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
 });
