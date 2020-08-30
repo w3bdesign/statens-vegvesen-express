53358d2e065441b9cef5e4d6fe6be98ad40d61b0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-30 04:30:10 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,43 @@
 /* eslint-disable no-unused-vars */
 
 import { NowRequest, NowResponse } from '@vercel/node';
+import express from 'express';
+import helmet from 'helmet';
+import fetch from 'node-fetch';
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+// const cors = require('cors');
 
 export default function Index(req: NowRequest, res: NowResponse) {
-  const { name = 'World' } = req.query;
-  res.send(`Hello ${name}!`);
+  const { nummer = 'World' } = req.query;
+
+  const urlToFetch = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${nummer}`;
+  if (nummer) {
+    fetch(urlToFetch)
+      .then((response) => response.json())
+      .then((data) => {
+        res.send(data);
+      });
+  }
 }
+
+// res.send(`Hello ${name}!`);
+// app.use(helmet());
+// app.use(cors(corsOptions));
+// app.listen(port);
+
+/* app.get('/bil/:nummer', (req, res, next) => {
+    const urlToFetch = `https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/kjoretoyoppslag/v1/kjennemerkeoppslag/kjoretoy/${req.params.nummer}`;
+    if (req.params.nummer) {
+      fetch(urlToFetch)
+        .then((response) => response.json())
+        .then((data) => {
+          res.send(data);
+          next();
+        })
+        .catch(() => {
+          throw new Error('Error');
+        });
+    }
+  }); */
